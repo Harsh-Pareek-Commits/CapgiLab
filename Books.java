297900d90e0edc6ed48da211eb70cfbf742082ed
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-04-19 18:46:09 +0530

    Add files via upload

---
+++ Books.java
@@ -0,0 +1,21 @@
+package lab4.lab4_3;
+
+public class Books extends WrittenItem {
+
+	public Books() {
+		super();
+	}
+
+	@Override
+	public void print(Item i) {
+		// TODO Auto-generated method stub
+		super.print(i);
+	}
+
+	@Override
+	public void additem(int uid, String title, int no_copies, String author) {
+		
+		super.additem(uid, title, no_copies, author);
+	}
+
+}
